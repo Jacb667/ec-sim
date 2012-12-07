408d5c8fce9514fb574dda425e995f81c0570a4e
jacb667 <jacb667@gmail.com>
2012-12-07 12:05:32 +0000

    Constants.java eliminado y movido a Global.

--- src/general/Global.java
+++ src/general/Global.java
@@ -1,6 +1,10 @@
 package general;
 
 public class Global {
+	
+	final static public int LONGITUD_BITS = 32;
+	final static public int TAMAÑO_MEMORIA = 1024;
+	final static public int TAMAÑO_BANCO = 32;
 
 	public enum Intrucciones
 	{
