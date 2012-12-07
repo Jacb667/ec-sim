2510e0c15cebec50accfad6eb4a12e8aa9df735b
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2012-12-07 20:13:45 +0000

    * Interface para caché.

--- src/pckMemoria/Cache.java
+++ src/pckMemoria/Cache.java
@@ -2,19 +2,22 @@ package pckMemoria;
 
 public interface Cache {
 	
+	// IMPORTANTE! leerDato, guardarDato, leerLinea y guardarLinea NO comprueban si el dato está en caché.
+	// Es necesario que la jerarquía de memoria compruebe antes si el dato está en esta caché o no.
+	// En caso de que no esté, la jerarquía se encargará de traerlo desde la memoria principal.
+	
+	// Comprobaciones
 	public boolean existeDato(int direccion);
 	public boolean isDirty(int direccion);
-	public boolean isValid(int direccion);
 	
+	// Tamaño de línea.
 	public int getTamanoLinea();
 	
+	// Operaciones para datos individuales.
 	public int leerDato(int direccion);
-	public void guardarDato(int direccion, int dato);
+	public void guardarDato(int direccion, int dato, boolean setDirty);
 	
 	// Operaciones para líneas (cache)
 	public int[] leerLinea(int direccion);
-	public void guardarLinea(int direccion, int[] linea);
-	
-	public String toString(boolean mostrarTodos);
-
+	public void guardarLinea(int direccion, int[] linea, boolean setDirty);
 }
