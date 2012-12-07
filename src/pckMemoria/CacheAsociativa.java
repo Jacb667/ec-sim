14d18550f9dc2ac595f58aadc7563d36c1500789
jacb667 <jacb667@gmail.com>
2012-12-07 20:14:30 +0000

    * Terminada la Caché Directa (en teoría). * Actualizado Test. * Documentación.

--- src/pckMemoria/CacheAsociativa.java
+++ src/pckMemoria/CacheAsociativa.java
@@ -2,40 +2,68 @@ package pckMemoria;
 
 public class CacheAsociativa implements Cache
 {
+	
+	// Cada una de las vías se implementa como una caché directa.
+	// De modo que el diseño es bastante simple.
+	private CacheDirecta vias[];
+	
+	private int entradas;
+	private int palabras_linea;
+	
+	// En caché directa se recomienda usar tamaños de potencias de 2^x.
+	// En caché asociativa la división entradas/vías DEBE dar exacto (no decimales).
+	// También se recomienda que entradas sea potencia de 2 (y divisible entre vías).
+	// Estas comprobaciones deben ser echas antes de invocar a este constructor.
+	public CacheAsociativa(int _entradas, int _palabras_linea, int _vias)
+	{
+		entradas = _entradas / _vias;
+		palabras_linea = _palabras_linea;
+		
+		// Creamos el array de vías
+		vias = new CacheDirecta[_vias];
+		
+		// Creamos las vías
+		for (int i = 0; i < _vias; i++)
+		{
+			vias[i] = new CacheDirecta(entradas, palabras_linea);
+		}
+	}
+	
+	// Para saber si un dato está, comprobamos todas las vías.
+	public boolean existeDato(int direccion)
+	{
+		boolean res = false;
+		int via = 0;
+		while (!res && via < vias.length)
+			res = vias[via].equals(direccion);
+		
+		return res;
+	}
 
-	@Override
-	public boolean existeDato(int direccion) {
+	// Compruebo si isDirty en la vía que esté.
+	public boolean isDirty(int direccion)
+	{
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public boolean isDirty(int direccion) {
-		// TODO Auto-generated method stub
-		return false;
+	public int getTamanoLinea()
+	{
+		return palabras_linea;
 	}
 
-	@Override
-	public boolean isValid(int direccion) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	// Si esto se ejecuta es porque sabemos que el dato está (en alguna vía).
+	// Compruebo en qué vía está y leo el dato.
+	public int leerDato(int direccion)
+	{
 
-	@Override
-	public int getTamanoLinea() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
-	public int leerDato(int direccion) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void guardarDato(int direccion, int dato) {
-		// TODO Auto-generated method stub
+	// Si esto se ejecuta es porque sabemos que el bloque del dato está (en alguna vía).
+	// Compruebo en qué vía está y guardo el dato.
+	public void guardarDato(int direccion, int dato, boolean setDirty)
+	{
 		
 	}
 
@@ -46,15 +74,9 @@ public class CacheAsociativa implements Cache
 	}
 
 	@Override
-	public void guardarLinea(int direccion, int[] linea) {
+	public void guardarLinea(int direccion, int[] linea, boolean setDirty) {
 		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	public String toString(boolean mostrarTodos) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
