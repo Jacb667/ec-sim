6b56dd20b0c6d0318132ff6c05d17b961ed3f579
alvarezgaonamanuel@gmail.com <alvarezgaonamanuel@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2012-12-08 14:09:22 +0000

    Interfaz grafica base con un main

---
+++ src/interfazgrafica/Interfaz.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Manuel
+ */
+public class Interfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJPanel p=new NewJPanel();
+        JFrame ventana=new JFrame("Programa");
+        ventana.setContentPane(p);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.pack();
+        ventana.setVisible(true);
+        
+    }
+}
