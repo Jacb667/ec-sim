14d77929322b24820e94b6adfcfd2eed1334ff78
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2012-12-09 21:03:32 +0000

    * Test para probar las cachés. * Global.

--- src/general/Test.java
+++ src/general/Test.java
@@ -1,5 +1,7 @@
 package general;
 
+import general.Global.PoliticasReemplazo;
+
 import java.util.Arrays;
 
 import pckMemoria.Cache;
@@ -45,38 +47,49 @@ public class Test {
 		
 		System.out.println(m2.toString(true));
 		
-		Cache c = new CacheDirecta(16, 4);
+		/*Cache c = new CacheDirecta(16, 4);
 		for (int i = 0; i < 16*4*4; i+=4)
-			c.guardarDato(i, i * 2, false);
+			c.escribirLinea(i, new int[]{1,2,3,4});
 		
 		System.out.println(c.toString());
 		
-		System.out.println(c.leerDato(0x04));
+		System.out.println(c.consultarDato(0x04));
 		
-		c.guardarLinea(0x40, m1.leerLinea(0x20, 4), true);
+		c.escribirLinea(0x40, m1.leerLinea(0x20, 4));
 		
 		System.out.println(c.toString());
 		
-		System.out.println("Dato 0x14: " + c.estaLibre(0x14) + c.existeDato(0x14) + c.isDirty(0x14));
+		System.out.println("Dato 0x14: " + c.lineaLibre(0x14) + c.existeDato(0x14) + c.lineaDirty(0x14));
 		if (c.existeDato(0x14))
-			System.out.println(c.leerDato(0x14));
-		System.out.println("Dato 0x4C: " + c.estaLibre(0x4C) + c.existeDato(0x4C) + c.isDirty(0x4C));
+			System.out.println(c.consultarDato(0x14));
+		System.out.println("Dato 0x4C: " + c.lineaLibre(0x4C) + c.existeDato(0x4C) + c.lineaDirty(0x4C));
 		if (c.existeDato(0x4C))
-			System.out.println(c.leerDato(0x4C));
-		System.out.println("Dato 0xE0: " + c.estaLibre(0xE0) + c.existeDato(0xE0) + c.isDirty(0xE0));
+			System.out.println(c.consultarDato(0x4C));
+		System.out.println("Dato 0xE0: " + c.lineaLibre(0xE0) + c.existeDato(0xE0) + c.lineaDirty(0xE0));
 		if (c.existeDato(0xE0))
-			System.out.println(c.leerDato(0xE0));
+			System.out.println(c.consultarDato(0xE0));*/
 		
-		/*System.out.println("Cache Asociativa: \n");
-		Cache c = new CacheAsociativa(32, 4, 4);
-		System.out.println(c.existeDato(0x20));
+		System.out.println("Cache Asociativa: \n");
+		Cache c = new CacheAsociativa(16, 4, 4, PoliticasReemplazo.RANDOM);
 		
-		for (int i = 0x24; i < 0x40; i+=4)
-			c.guardarDato(i, i*2, false);
+		c.escribirLinea(0x0000, new int[]{0,1,2,3});
+		c.escribirLinea(0xFF00, new int[]{99,100,101,102});
+		c.escribirLinea(0xCC00, new int[]{55,56,57,25});
 		
-		c.guardarDato(0x20, 76, true);
+		c.modificarDato(0x20, 76);
 		
-		System.out.println(c.toString());*/
+		System.out.println(c.toString());
+		
+		System.out.println(c.existeDato(0x04));
+		System.out.println(c.consultarDato(0x04));
+		
+		System.out.println(c.existeDato(0xFF04));
+		System.out.println(c.consultarDato(0xFF04));
+		
+		c.modificarDato(0xFF04, 900);
+		
+		System.out.println(c.toString());
+		//c.modificarDato(0x14, 0, true);
 		
 		//System.out.println("Leer: " + Arrays.toString(c.leerLinea(0xFF)));
 	}
