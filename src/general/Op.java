59190396ebd66037aaf0a46b37fa95d14c15dde3
jacb667 <jacb667@gmail.com>
2012-12-16 22:28:19 +0000

    * Corregida función para calcular bits necesarios, utilizando logaritmo en base 2.

--- src/general/Op.java
+++ src/general/Op.java
@@ -15,16 +15,10 @@ public class Op
 	// Devuelve el número de bits necesarios para direccionar i posiciones
 	public static int bitsDireccionar(int i)
 	{
-		// No es necesario direccionar 1 sólo bit, ni menos de 1.
-		if (i <= 1)
-			return 0;
-
-		// Si el número es par le resto 1 para evitar límites.
-		if (i % 2 == 0)
-			i -= 1;
-		
-		String aux = Integer.toBinaryString(i);
-		return aux.length();
+		// Logaritmo de i en base 2. Redondeado siempre hacia arriba (si
+		// sobrepasa una única posición, ya es necesario un bit más para
+		// direccionar).
+		return (int) Math.ceil((Math.log(i) / Math.log(2)));
 	}
 	
 	
