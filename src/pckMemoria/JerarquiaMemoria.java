6018793df4305768279b763e867fb4669b070f47
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2012-12-17 14:22:17 +0000

    * pckInterfaz repetido eliminado. * Jerarquia -> JerarquiaMemoria.

--- src/pckMemoria/JerarquiaMemoria.java
+++ src/pckMemoria/JerarquiaMemoria.java
@@ -3,20 +3,24 @@ package pckMemoria;
 import general.Global.TiposReemplazo;
 
 // Jerarquía de memoria
-public class Jerarquia {
+public class JerarquiaMemoria {
 	
 	private Cache caches[];
 	private MemoriaPrincipal memoria;
 	
 	
-	public Jerarquia(Cache[] _caches, MemoriaPrincipal _memoria)
+	public JerarquiaMemoria(Cache[] _caches, MemoriaPrincipal _memoria)
 	{
 		System.arraycopy(_caches, 0, caches, 0, _caches.length);
 		memoria = _memoria;
-		
-		
-		
+	}
+	
+	
+	// Leer un dato.
+	public int leerDato(int direccion)
+	{
 		
 	}
+	
 
 }
