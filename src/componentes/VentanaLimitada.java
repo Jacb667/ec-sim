2d119c314a839b8bd2aba6bf8ecb99b0957f7f96
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2012-12-30 01:24:14 +0000

    * Añado 2 nuevas clases para gestión de ventanas personalizada.

---
+++ src/componentes/VentanaLimitada.java
@@ -0,0 +1,30 @@
+package componentes;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JFrame;
+
+public class VentanaLimitada extends JFrame {
+	
+	// Esta ventana está limitada a unas dimensiones mínimas y máximas.
+	@Override
+	public void paint(Graphics g)
+	{
+		Dimension d = getSize();
+		Dimension m = getMaximumSize();
+		boolean resize = d.width > m.width || d.height > m.height;
+		d.width = Math.min(m.width, d.width);
+		d.height = Math.min(m.height, d.height);
+		if (resize)
+		{
+			Point p = getLocation();
+			setVisible(false);
+			setSize(d);
+			setLocation(p);
+			setVisible(true);
+		}
+		super.paint(g);
+	}
+}
