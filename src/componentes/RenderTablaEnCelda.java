1a5c7151701b267e6607cbbed6d0d62237d873ab
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2012-12-31 00:29:36 +0000

    * Terminada tabla y RenderTablaEnCelda.

---
+++ src/componentes/RenderTablaEnCelda.java
@@ -0,0 +1,58 @@
+package componentes;
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+
+public class RenderTablaEnCelda implements TableCellRenderer {
+	
+	/* Magic Happens */
+	public Component getTableCellRendererComponent(JTable table,
+			Object value, boolean isSelected, boolean hasFocus,
+			int row, int column)
+	{
+		if (value == null)
+			return null;
+		
+		/* If what we're displaying isn't an array of values we
+		return the normal renderer*/
+		if (!value.getClass().isArray())
+		{
+			return table.getDefaultRenderer(
+					value.getClass()).getTableCellRendererComponent(
+							table, value, isSelected, hasFocus,row, column);
+		}
+		else
+		{
+			final Object[] passed = (Object[])value;
+
+			// Obtenemos el tamaño actual de esta línea.
+			int alturaNormal = table.getRowHeight();
+			int alturaActual = table.getRowHeight(row);
+			int alturaNueva = passed.length * alturaNormal;
+			
+			if (alturaActual < alturaNueva)
+				table.setRowHeight(row, alturaNueva);
+			
+			/* We create the table that will hold the multivalue
+			 *fields and that will be embedded in the main table */
+			return new JTable( new AbstractTableModel() 
+			{
+				public int getColumnCount() { return 1; }
+				public int getRowCount() { return passed.length; }
+				public Object getValueAt(int rowIndex, int columnIndex) {
+					return passed[rowIndex]; }
+				public boolean isCellEditable(int row, int col){ return true; }
+				public Class<? extends Object> getColumnClass(int c)
+				{
+					Object valor = getValueAt(0, c);
+					if (valor != null)
+						return valor.getClass();
+					else
+						return new String().getClass();
+			    }
+			});
+		}
+    }
+}
--- src/componentes/Tabla.java
+++ src/componentes/Tabla.java
@@ -1,10 +1,19 @@
 package componentes;
 
 import javax.swing.JTable;
+import javax.swing.JViewport;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
+import pckMemoria.Cache;
+import pckMemoria.MemoriaPrincipal;
 
 @SuppressWarnings("serial")
 public class Tabla extends JTable {
+	
+	private final int TAMAÑO_DEFECTO = 60;
+	private final int TAMAÑO_BOOLEAN = 40;
 	
 	// Nuestra propia JTable, ya que necesito realizar algunas modificaciones.
 	public Tabla(Object[][] data, Object[] columns)
@@ -13,6 +22,19 @@ public class Tabla extends JTable {
 		init();
 	}
 	
+	public Tabla(MemoriaPrincipal memoria)
+	{
+		super(memoria.getDatos(), memoria.getColumnas());
+		init();
+	}
+	
+	public Tabla(Cache cache)
+	{
+		super(cache.getDatos(), cache.getColumnas());
+		init();
+	}
+	
+	// Inicializar
 	private void init()
 	{
 		//setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
@@ -22,9 +44,7 @@ public class Tabla extends JTable {
 		setOpaque(false);
 		
 		// Bloqueo la redimensión de las columnas.
-		tamañoColumna(0, 100);
-		tamañoColumna(1, 100);
-		tamañoColumna(2, 50);
+		tamañoColumnas();
 		
 		// Desactivamos la selección de filas y columnas.
 		setRowSelectionAllowed(false);
@@ -38,18 +58,56 @@ public class Tabla extends JTable {
 		return false;
 	}
 	
-	// Esta sirve para que salga el Checkbox en la columna booleana.
-	public Class getColumnClass(int c)
+	// Esta clase sirve para solucionar un bug en las JTable, que
+	// no aparece el scroll horizontal siempre que debería.
+	public boolean getScrollableTracksViewportWidth()
 	{
-        return getValueAt(0, c).getClass();
+		if (autoResizeMode != AUTO_RESIZE_OFF)
+		{
+			if (getParent() instanceof JViewport)
+				return (((JViewport)getParent()).getWidth() > getPreferredSize().width);
+		}
+		return false;
+	}
+	
+	// Esta sirve para que salga el Checkbox en la columna booleana.
+	public Class<? extends Object> getColumnClass(int c)
+	{
+		Object valor = getValueAt(0, c);
+		if (valor != null)
+			return valor.getClass();
+		else
+			return new String().getClass();
     }
 	
-	private void tamañoColumna(int c, int tam)
+	// Inicializa el tamaño de las columnas dependiendo del tipo de dato.
+	private void tamañoColumnas()
 	{
-		TableColumn columna = getColumnModel().getColumn(c);
-		columna.setMinWidth(tam);
-		//columna.setMaxWidth(tam);
-		columna.setPreferredWidth(tam);
+		for (int i = 0; i < getColumnCount(); i++)
+		{
+			TableColumn columna = getColumnModel().getColumn(i);
+			if (getColumnClass(i) == Boolean.class)
+			{
+				columna.setMinWidth(TAMAÑO_BOOLEAN);
+				columna.setMaxWidth(TAMAÑO_BOOLEAN*2);
+				columna.setPreferredWidth(TAMAÑO_BOOLEAN);
+			}
+			else
+			{
+				columna.setMinWidth(TAMAÑO_DEFECTO);
+				columna.setPreferredWidth(TAMAÑO_DEFECTO);
+			}
+		}
 	}
-
+	
+	public void setRenderTablaEnCelda()
+	{
+		TableCellRenderer jTableCellRenderer = new RenderTablaEnCelda();
+		
+		TableColumnModel tcm = getColumnModel();
+		for(int it = 0; it < tcm.getColumnCount(); it++)
+		{
+			tcm.getColumn(it).setCellRenderer(jTableCellRenderer);
+		}
+	}
 }
