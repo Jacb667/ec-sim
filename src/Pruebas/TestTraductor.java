27f2f777ac088da86de1654b95d0ebbf44c06590
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2013-01-10 00:01:13 +0000

    * Un par de pruebas más.

--- src/Pruebas/TestTraductor.java
+++ src/Pruebas/TestTraductor.java
@@ -12,12 +12,14 @@ public class TestTraductor {
 	{
 		try
 		{
-			TablaPaginas tablaPags = new TablaPaginas(1024, palabras_linea, 4294967295l, 10240, TiposReemplazo.RANDOM);
+			//TablaPaginas tablaPags = new TablaPaginas(4096, palabras_linea, 4294967295l, 40960, TiposReemplazo.RANDOM);
+			TablaPaginas tablaPags = new TablaPaginas(16, palabras_linea, 512, 64, TiposReemplazo.RANDOM);
 			System.out.println(tablaPags);
 			
 			// Direcciones entre 0 y 1024 -> Página 0
 			
-			System.out.println(tablaPags.traducirDireccion(4096));
+			System.out.println("2047 traducida como: 0x" + Integer.toHexString(tablaPags.traducirDireccion(2047)));
+			
 		}
 		catch (MemoryException e)
 		{
