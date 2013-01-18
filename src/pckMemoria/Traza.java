c0fe85377a2748494a816b5d9b44ee472dc594cc
alvarezgaonamanuel@gmail.com <alvarezgaonamanuel@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2013-01-18 18:58:36 +0000

    * Traza terminada.

--- src/pckMemoria/Traza.java
+++ src/pckMemoria/Traza.java
@@ -1,16 +1,31 @@
 package pckMemoria;
 
+import general.Log;
+import general.MemoryException;
+
 import java.util.*;
 
 public class Traza {
+	int TAM=3;
 
 	int contLines=0;
 	int dirInst;
 	int dirMem;
 	String func;
-	String result="";
+	
+	int ultAP=0;
+	int ultAT=0;
+	int[] ultAC= new int[3];
+	
+	JerarquiaMemoria jmem, jins;
+	StringBuilder stb=new StringBuilder();
+	public Traza(JerarquiaMemoria j1, JerarquiaMemoria j2)
+	{
+		jmem=j1;
+		jins=j2;
+	}
 
-	public void readLines(String s)//desde un string
+	public void readLines(String s) throws MemoryException//desde un string
 	{
 		StringTokenizer str = new StringTokenizer(s,"'\n'");
 		while(str.hasMoreTokens())
@@ -21,7 +36,7 @@ public class Traza {
 		}
 	}
 	
-	public void decLine(String s)
+	public void decLine(String s) throws MemoryException
 	{
 		StringTokenizer str=new StringTokenizer(s,",.; ");
 		int strcount=str.countTokens();
@@ -33,13 +48,13 @@ public class Traza {
 				func=str.nextToken().toUpperCase();
 				if((dirInst<0)||(dirMem<0))
 				{
-					System.out.println("Error en la linea "+contLines+ " no se aceptan numeros negativos");
+					throw new MemoryException("Error en la linea "+contLines+ " no se aceptan numeros negativos");
 				}
-				System.out.println("es de dos " + func);
+				ejecutarLineaF(dirInst);
 			}
 			catch (NumberFormatException e)
 			{
-				System.out.println("Error de formato en la linea "+contLines);
+				throw new MemoryException("Error de formato en la linea "+contLines);
 			}
 		}
 		else if(strcount==3)
@@ -49,30 +64,109 @@ public class Traza {
 				dirInst=Integer.parseInt(str.nextToken());
 				func=str.nextToken().toUpperCase();
 				dirMem=Integer.parseInt(str.nextToken());
-				System.out.println("es de tres");
 				if((dirInst<0)||(dirMem<0))
 				{
-					System.out.println("Error en la linea "+contLines);
+					throw new MemoryException("Error en la linea "+contLines);
 				}
+				ejecutarLineaF(dirInst);
+				ejecutarLineaRW(dirMem, func);
 			}
 			catch (NumberFormatException e)
 			{
-				System.out.println("Error de formato en la linea "+contLines);
+				throw new MemoryException("Error de formato en la linea "+contLines);
 			}
 		}
 		else
 		{
-			System.out.println("Error en la linea "+contLines +" formato no aceptado");
+			throw new MemoryException("Error en la linea "+contLines +" formato no aceptado");
+		}
+		
+		
+	}
+	public void ejecutarLineaF(int dirInst) throws MemoryException
+	{
+		if(jins==null)
+		{
+			Direccion d=jmem.simularLeerDato(dirInst);
+			stb.append("FETCH--\t");
+			generarEstadistica(d);
+		}
+		else
+		{
+			Direccion d=jins.simularLeerDato(dirInst);
+			stb.append("FETCH--\t");
+			generarEstadistica(d);
+		}
+		
+	}
+
+	private void generarEstadistica(Direccion d) {
+		
+		if(ultAT!=Log.accesosTlb)
+		{
+			ultAT=Log.accesosTlb;
+			stb.append("TLB: HIT, ");
+			
+		}
+		else
+		{
+			stb.append("TLB: MISS, ");
+		}
+		if(ultAP!=Log.aciertosPagina)
+		{
+			ultAP=Log.aciertosPagina;
+			stb.append("PAGE: "+d.getPagina()+" HIT, Direccion Fisica: "+d.getReal()+" ");
+		}
+		else
+		{
+			stb.append("PAGE: "+d.getPagina()+" FAULT, Direccion Fisica: "+d.getReal()+" ");
+		}
+		
+		for(int i=0;i<jmem.getNivelesCache();i++)
+		{
+			if(ultAC[i]!=Log.cache_hits[i])
+			{
+				stb.append("Cache "+i+": HIT ");
+				ultAC[i]=Log.cache_hits[i];
+				break;
+			}
+			else
+			{
+				stb.append("Cache "+i+": MISS ");
+			}
+		}
+		
+	
+		stb.append(" Linea: "+contLines+"\n");
+	}
+	public void ejecutarLineaRW(int dirMem, String s) throws MemoryException
+	{
+		
+		if(s.equals("W"))
+		{
+			stb.append("W--\t");
+			Direccion d=jmem.simularGuardarDato(dirMem, dirMem);
+			generarEstadistica(d);
+		}
+		else if(s.equals("R"))
+		{
+			stb.append("R--\t");
+			Direccion d=jmem.simularLeerDato(dirMem);
+			generarEstadistica(d);
+		}
+		else
+		{
+			throw new MemoryException("Parametro invalido: "+s+" En linea: "+contLines);
 		}
 	}
 	
 	public void addToResult(String s)
 	{
-		StringBuilder stb=new StringBuilder();
-		stb.append(result).append(s).append("/n");
+		
+		stb.append(s).append("\n");
 	}
 	public String getResult()
 	{
-		return result;
+		return stb.toString();
 	}
 }
