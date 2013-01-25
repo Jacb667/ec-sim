62d84bbb6dd8e4a12201787e2f7509550f343ee4
jacb667 <jacb667@gmail.com>
2013-01-25 23:47:08 +0000

    * Más segmentacion.

--- src/pckCpu/RegistroSegmentacion.java
+++ src/pckCpu/RegistroSegmentacion.java
@@ -5,24 +5,22 @@ public class RegistroSegmentacion {
 	private Instruccion instruccion;
 	private int data1;
 	private int data2;
-	private int destino;
 	private int valor;
-	private int[] flags;
+	private boolean[] flags;
 	
 	public RegistroSegmentacion() { }
 
 	public Instruccion getInstruccion() { return instruccion; }
 	public int getData1() { return data1; }
 	public int getData2() { return data2; }
-	public int getDestino() { return destino; }
+	public int getDestino() { return instruccion.getDestino(); }
 	public int getValor() { return valor; }
-	public int[] getFlags() { return flags; }
+	public boolean[] getFlags() { return flags; }
 
 	public void setInstruccion(Instruccion instruccion) { this.instruccion = instruccion; }
 	public void setData1(int data1) { this.data1 = data1; }
 	public void setData2(int data2) { this.data2 = data2; }
-	public void setDestino(int destino) { this.destino = destino; }
 	public void setValor(int valor) { this.valor = valor; }
-	public void setFlags(int[] flags) { this.flags = flags; }
+	public void setFlags(boolean[] flags) { this.flags = flags; }
 
 }
