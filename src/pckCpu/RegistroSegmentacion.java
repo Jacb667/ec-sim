f6ab427dc10ed2adcdc4515dd9b91396ab124e99
jacb667 <jacb667@gmail.com>
2013-01-25 21:58:20 +0000

    * Campo flags.

--- src/pckCpu/RegistroSegmentacion.java
+++ src/pckCpu/RegistroSegmentacion.java
@@ -7,6 +7,7 @@ public class RegistroSegmentacion {
 	private int data2;
 	private int destino;
 	private int valor;
+	private int[] flags;
 	
 	public RegistroSegmentacion() { }
 
@@ -15,11 +16,13 @@ public class RegistroSegmentacion {
 	public int getData2() { return data2; }
 	public int getDestino() { return destino; }
 	public int getValor() { return valor; }
+	public int[] getFlags() { return flags; }
 
 	public void setInstruccion(Instruccion instruccion) { this.instruccion = instruccion; }
 	public void setData1(int data1) { this.data1 = data1; }
 	public void setData2(int data2) { this.data2 = data2; }
 	public void setDestino(int destino) { this.destino = destino; }
 	public void setValor(int valor) { this.valor = valor; }
+	public void setFlags(int[] flags) { this.flags = flags; }
 
 }
