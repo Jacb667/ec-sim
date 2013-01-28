a785253d0e877d429c963d6f5e79947a06c63ad1
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2013-01-28 18:37:29 +0000

    * Config

--- src/general/Config.java
+++ src/general/Config.java
@@ -4,16 +4,53 @@ package general;
 
 public class Config {
 	
-	private static int c[] = new int[Conf_Type.END_CONFIG.index()];
+	private static int enteros[] = new int[Conf_Type.END_CONFIG.index()];
+	private static String cadenas[] = new String[Conf_Type_c.END_CONFIG.index()];
 	
 	public static void set(Conf_Type config, int value)
 	{
-		c[config.index()] = value;
+		enteros[config.index()] = value;
 	}
 	
 	public static int get(Conf_Type config)
 	{
-		return c[config.index()];
+		return enteros[config.index()];
+	}
+	
+	public static void set(Conf_Type_c config, String value)
+	{
+		cadenas[config.index()] = value;
+	}
+	
+	public static String get(Conf_Type_c config)
+	{
+		return cadenas[config.index()];
+	}
+	
+	public enum Conf_Type_c
+	{
+		TLB_DATOS_POLITICA(0),
+		TLB_INSTRUCCIONES_POLITICA(1),
+		CACHE1_DATOS_POLITICA(2),
+		CACHE2_DATOS_POLITICA(3),
+		CACHE3_DATOS_POLITICA(4),
+		CACHE1_INSTRUCCIONES_POLITICA(5),
+		CACHE2_INSTRUCCIONES_POLITICA(6),
+		CACHE3_INSTRUCCIONES_POLITICA(7),
+		ARCHIVO_TRAZA(8),
+		ARCHIVO_CODIGO(9),
+		ARCHIVO_EXPORTAR(10),
+		END_CONFIG (11);
+		
+		private int index;   
+
+		Conf_Type_c(int index) {
+	        this.index = index;
+	    }
+
+	    public int index() { 
+	        return index; 
+	    }
 	}
 	
 	public enum Conf_Type
@@ -28,35 +65,24 @@ public class Config {
 		TLB_INSTRUCCIONES(7),
 		TLB_DATOS_ENTRADAS(8),
 		TLB_DATOS_VIAS(9),
-		TLB_DATOS_POLITICA(10),
-		TLB_INSTRUCCIONES_ENTRADAS(11),
-		TLB_INSTRUCCIONES_VIAS(12),
-		TLB_INSTRUCCIONES_POLITICA(13),
-		NIVELES_CACHE_DATOS(14),
-		NIVELES_CACHE_INSTRUCCIONES(15),
-		TAMAÑO_LINEA(16),
-		CACHE1_DATOS_ENTRADAS(17),
-		CACHE1_DATOS_VIAS(18),
-		CACHE1_DATOS_POLITICA(19),
-		CACHE2_DATOS_ENTRADAS(20),
-		CACHE2_DATOS_VIAS(21),
-		CACHE2_DATOS_POLITICA(22),
-		CACHE3_DATOS_ENTRADAS(23),
-		CACHE3_DATOS_VIAS(24),
-		CACHE3_DATOS_POLITICA(25),
-		CACHE1_INSTRUCCIONES_ENTRADAS(26),
-		CACHE1_INSTRUCCIONES_VIAS(27),
-		CACHE1_INSTRUCCIONES_POLITICA(28),
-		CACHE2_INSTRUCCIONES_ENTRADAS(29),
-		CACHE2_INSTRUCCIONES_VIAS(30),
-		CACHE2_INSTRUCCIONES_POLITICA(31),
-		CACHE3_INSTRUCCIONES_ENTRADAS(32),
-		CACHE3_INSTRUCCIONES_VIAS(33),
-		CACHE3_INSTRUCCIONES_POLITICA(34),
-		ARCHIVO_TRAZA(35),
-		ARCHIVO_CODIGO(36),
-		ARCHIVO_EXPORTAR(37),
-		END_CONFIG (38);
+		TLB_INSTRUCCIONES_ENTRADAS(10),
+		TLB_INSTRUCCIONES_VIAS(11),
+		NIVELES_CACHE_DATOS(12),
+		NIVELES_CACHE_INSTRUCCIONES(13),
+		TAMAÑO_LINEA(14),
+		CACHE1_DATOS_ENTRADAS(15),
+		CACHE1_DATOS_VIAS(16),
+		CACHE2_DATOS_ENTRADAS(17),
+		CACHE2_DATOS_VIAS(18),
+		CACHE3_DATOS_ENTRADAS(19),
+		CACHE3_DATOS_VIAS(20),
+		CACHE1_INSTRUCCIONES_ENTRADAS(21),
+		CACHE1_INSTRUCCIONES_VIAS(22),
+		CACHE2_INSTRUCCIONES_ENTRADAS(23),
+		CACHE2_INSTRUCCIONES_VIAS(24),
+		CACHE3_INSTRUCCIONES_ENTRADAS(25),
+		CACHE3_INSTRUCCIONES_VIAS(26),
+		END_CONFIG (27);
 		
 		private int index;   
 
