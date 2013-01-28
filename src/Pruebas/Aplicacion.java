7c4d35443b3e045a77cb65e2cdc7c7bf49809aed
jacb667@gmail.com <jacb667@gmail.com@e89de405-67f2-c004-578d-39e6706204ad>
2013-01-28 22:52:22 +0000

    * Tildes y ñ en la interfaz. * Visual.

--- src/Pruebas/Aplicacion.java
+++ src/Pruebas/Aplicacion.java
@@ -8,6 +8,8 @@ import interfazgrafica.Controlador;
 import interfazgrafica.Vista;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 
 /**
@@ -18,10 +20,11 @@ public class Aplicacion extends javax.swing.JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
-    public static void main(String args[]) {
+    public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+    	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     	 Vista v=new Vista();
-         JFrame ventana=new JFrame("Programa");
-         Controlador ctr= new Controlador(v);
+         JFrame ventana = new JFrame("Simulador MIPS v1.1");
+         Controlador ctr = new Controlador(v);
          v.controlador(ctr);
          ventana.setContentPane(v);
          ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,7 +36,7 @@ public class Aplicacion extends javax.swing.JFrame {
         //new Test();
     	//new TestMemoria();
     	//new TestMemoria2();
-    	new TestCpu();
+    	//new TestCpu();
     	//new TestTraza();
     	//new TestTraductor();
     }
